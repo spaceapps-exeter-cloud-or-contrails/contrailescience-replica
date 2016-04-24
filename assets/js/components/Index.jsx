dd24a334e0585f9fa65747a1b573540c37e59410
Decebal Dobrica <decebal.dobrica@gmail.com>
2016-04-24 08:28:10 +0100

    update base template

---
+++ assets/js/components/Index.jsx
@@ -0,0 +1,11 @@
+// React components
+var React = 'react';
+var ReactMDL = 'react-mdl';
+
+var Button = ReactMDL.Button;
+var Card = ReactMDL.Card
+
+React.render(
+   document.body
+);
+
--- views/Default.jsx
+++ views/Default.jsx
@@ -2,7 +2,6 @@
 var React = require('react');
 
 var Default = React.createClass({
-
   render: function() {
 
     return(
@@ -11,19 +10,15 @@ var Default = React.createClass({
 
         <meta charSet="utf-8"></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
-        <title>Todo App</title>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet"></link>
-        <link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'></link>
+        <title>Contrail App</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet"></link>      
 
       </head>
       <body style={{fontFamily: ['Varela Round', 'sans-serif']}}>
-        <div id="map" style={{width: 600px; height: 400px}}></div>
-
-        <script src="sample-geojson.js" type="text/javascript"></script>
-        <script src="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js"></script>
 
         <div id="app"></div>
         <script src="js/bundle.js"></script>
+
       </body>
       </html>
 
